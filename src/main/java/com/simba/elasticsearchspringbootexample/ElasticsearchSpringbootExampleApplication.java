732753dc2ea62b2a2f54f9ea932ec3581064d718
elhadjiomar <simbadeveloppement@gmail.com>
2019-05-01 07:12:06 +0000

    Initial commit

---
+++ src/main/java/com/simba/elasticsearchspringbootexample/ElasticsearchSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.simba.elasticsearchspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchSpringbootExampleApplication.class, args);
+	}
+
+}
